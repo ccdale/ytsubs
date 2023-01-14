f336e95bc9688769fd9f19c2b432b105b43879e6
ccdale <chris.allison@hivehome.com>
2023-01-14 07:42:21 +0000

    Bumping version from 0.1.15 to 0.1.16

--- src/ytsubs/__init__.py
+++ src/ytsubs/__init__.py
@@ -4,7 +4,7 @@ import ccalogging
 
 log = ccalogging.log
 
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 __appname__ = "ytsubs"
 
 
--- tests/test_ytsubs.py
+++ tests/test_ytsubs.py
@@ -2,4 +2,4 @@ from ytsubs import __version__
 
 
 def test_ytsubs_version():
-    assert __version__ == "0.1.15"
+    assert __version__ == "0.1.16"
